3fd7ec395599a427cb750295ac63f1266245c41d
kyan820815 <kyan820815>
2021-09-08 18:17:10 +0800

    20210908-1 add readme and user manual

--- README.md
+++ README.md
@@ -1,3 +1,32 @@
 # Go automate for BrowserStack
 
 https://automate.browserstack.com/
+
+This is the automation for [BrowserStack](https://www.browserstack.com/).
+Users can use this package to automate visiting any websites for any os and browsers for testing.
+
+Below are five parameters for users to set in .env as their accounts of BrowserStack and Url that they would visit.
+
+## Parameters
+* <strong>BROWSERSTACK_USERNAME</strong>: BrowserStack user name
+* <strong>BROWSERSTACK_ACCESS_KEY</strong>: BrowserStack access key
+* <strong>BROWSERSTACK_URL</strong>: the url address users will visit
+* <strong>BROWSERSTACK_HOSTS</strong>: binding of IP address and DNS
+* <strong>BROWSERSTACK_RUN_LOCAL</strong>: bo0lean value, true if users want to visit interanl network for local teseting.
+For the format, please refer to [.env.example](./env.example)
+
+## Run Locally
+If users want to visit internal address, e.g., http://localhost:8080, users need to start the local connection first.
+
+* First visit here(https://www.browserstack.com/local-testing/app-automate) and go to <strong>command-line interface part</strong>
+* Follow the instruction to install and execute the execution file by the following command
+```
+./BrowserStackLocal --key BROWSERSTACK_ACCESS_KEY
+```
+* Then users can execute the package to perform local testing.
+
+
+## Run
+```
+make  
+```
--- main.go
+++ main.go
@@ -1,52 +1,51 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"os"
+    "fmt"
+    "log"
+    "os"
 
-	"github.com/airdb/go-browserstack/plugin"
+    "github.com/airdb/go-browserstack/plugin"
 
-	"github.com/joho/godotenv"
-	"github.com/tebeka/selenium"
+    "github.com/joho/godotenv"
+    "github.com/tebeka/selenium"
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
+    err := godotenv.Load()
+    if err != nil {
+        log.Fatal("Error loading .env file")
+    }
 
-	browserstackUsername := os.Getenv("BROWSERSTACK_USERNAME")
-	browserstackAccessKey := os.Getenv("BROWSERSTACK_ACCESS_KEY")
-	browserstackURL := os.Getenv("BROWSERSTACK_URL")
-	browserstackHosts := os.Getenv("BROWSERSTACK_HOSTS")
+    browserstackUsername := os.Getenv("BROWSERSTACK_USERNAME")
+    browserstackAccessKey := os.Getenv("BROWSERSTACK_ACCESS_KEY")
+    browserstackURL := os.Getenv("BROWSERSTACK_URL")
+    browserstackHosts := os.Getenv("BROWSERSTACK_HOSTS")
 
-	remoteUrl := fmt.Sprintf("https://%s:%s@hub-cloud.browserstack.com/wd/hub", browserstackUsername, browserstackAccessKey)
+    remoteUrl := fmt.Sprintf("https://%s:%s@hub-cloud.browserstack.com/wd/hub", browserstackUsername, browserstackAccessKey)
 
-	// device:https://www.browserstack.com/list-of-browsers-and-platforms/automate
-	// https://www.browserstack.com/automate/capabilities
-	// https://www.browserstack.com/docs/automate/selenium/select-browsers-and-devices#selenium-capabilities
-	//	"browserstack.local": "true",
-	caps := selenium.Capabilities{
-		"build":   "auto-test",
-		"name":    "Windows testing",
-		"project": "auto-test",
-		// "browserstack.debug":  "true",
-		// "browserstack.local":  "true",
-		// "browserstack.tunnel": "true",
-		"browserName":        "firefox",
-		"browserVersion":     "88.0",
-		"os":                 "Windows",
-		"os_version":         "8.1",
-		"browserstack.hosts": browserstackHosts,
-	}
+    // device:https://www.browserstack.com/list-of-browsers-and-platforms/automate
+    // https://www.browserstack.com/automate/capabilities
+    // https://www.browserstack.com/docs/automate/selenium/select-browsers-and-devices#selenium-capabilities
+    caps := selenium.Capabilities{
+        "build":   "auto-test",
+        "name":    "Windows testing",
+        "project": "auto-test",
+        // "browserstack.debug":  "true",
+        // "browserstack.local":  "true",
+        // "browserstack.tunnel": "true",
+        "browserName":        "firefox",
+        "browserVersion":     "88.0",
+        "os":                 "Windows",
+        "os_version":         "8.1",
+        "browserstack.hosts": browserstackHosts,
+    }
 
-	wd, err := selenium.NewRemote(caps, remoteUrl)
-	if err != nil {
-		panic(err)
-	}
-	defer wd.Quit()
+    wd, err := selenium.NewRemote(caps, remoteUrl)
+    if err != nil {
+        panic(err)
+    }
+    defer wd.Quit()
 
-	plugin.VisitLocal(wd, browserstackURL)
+    plugin.VisitLocal(wd, browserstackURL)
 }
