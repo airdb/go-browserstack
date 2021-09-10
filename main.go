937a3160cf6cbca7cd5b2c56899abd910fe18764
kyan820815 <kyan820815>
2021-09-10 10:37:47 +0800

    20210910-1 post data using url

--- main.go
+++ main.go
@@ -1,51 +1,70 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "os"
+	"fmt"
+	"log"
+	"os"
 
-    "github.com/airdb/go-browserstack/plugin"
+	"github.com/airdb/go-browserstack/plugin"
 
-    "github.com/joho/godotenv"
-    "github.com/tebeka/selenium"
+	"github.com/joho/godotenv"
+	"github.com/tebeka/selenium"
 )
 
 func main() {
-    err := godotenv.Load()
-    if err != nil {
-        log.Fatal("Error loading .env file")
-    }
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 
-    browserstackUsername := os.Getenv("BROWSERSTACK_USERNAME")
-    browserstackAccessKey := os.Getenv("BROWSERSTACK_ACCESS_KEY")
-    browserstackURL := os.Getenv("BROWSERSTACK_URL")
-    browserstackHosts := os.Getenv("BROWSERSTACK_HOSTS")
+	browserstackUsername := os.Getenv("BROWSERSTACK_USERNAME")
+	browserstackAccessKey := os.Getenv("BROWSERSTACK_ACCESS_KEY")
+	browserstackURL := os.Getenv("BROWSERSTACK_URL")
+	browserstackHosts := os.Getenv("BROWSERSTACK_HOSTS")
+	browserstackRunLocal := os.Getenv("BROWSERSTACK_RUN_LOCAL")
 
-    remoteUrl := fmt.Sprintf("https://%s:%s@hub-cloud.browserstack.com/wd/hub", browserstackUsername, browserstackAccessKey)
+	remoteUrl := fmt.Sprintf("https://%s:%s@hub-cloud.browserstack.com/wd/hub", browserstackUsername, browserstackAccessKey)
 
-    // device:https://www.browserstack.com/list-of-browsers-and-platforms/automate
-    // https://www.browserstack.com/automate/capabilities
-    // https://www.browserstack.com/docs/automate/selenium/select-browsers-and-devices#selenium-capabilities
-    caps := selenium.Capabilities{
-        "build":   "auto-test",
-        "name":    "Windows testing",
-        "project": "auto-test",
-        // "browserstack.debug":  "true",
-        // "browserstack.local":  "true",
-        // "browserstack.tunnel": "true",
-        "browserName":        "firefox",
-        "browserVersion":     "88.0",
-        "os":                 "Windows",
-        "os_version":         "8.1",
-        "browserstack.hosts": browserstackHosts,
-    }
+	// device:https://www.browserstack.com/list-of-browsers-and-platforms/automate
+	// https://www.browserstack.com/automate/capabilities
+	// https://www.browserstack.com/docs/automate/selenium/select-browsers-and-devices#selenium-capabilities
 
-    wd, err := selenium.NewRemote(caps, remoteUrl)
-    if err != nil {
-        panic(err)
-    }
-    defer wd.Quit()
+	browserName := "firefox"
+	browserVersion := "88.0"
+	os := "Windows"
+	osVersion := "8.1"
+	realMobile := "false"
+	device := ""
 
-    plugin.VisitLocal(wd, browserstackURL)
+	caps := selenium.Capabilities{
+		"build":              "auto-test",
+		"name":               "send testing",
+		"project":            "auto-test",
+		"browserstack.local": browserstackRunLocal,
+		// "browserstack.debug":  "true",
+		// "browserstack.tunnel": "true",
+		"browserstack.hosts": browserstackHosts,
+		"browserName":        browserName,    // for pc
+		"browserVersion":     browserVersion, // for pc
+		"os":                 os,             // for pc
+		"os_version":         osVersion,      // for pc and mobile
+		"RealMobile":         realMobile,     // for mobile
+		"Device":             device,         // for mobile
+	}
+
+	wd, err := selenium.NewRemote(caps, remoteUrl)
+	if err != nil {
+		panic(err)
+	}
+	defer wd.Quit()
+
+	requestURL := fmt.Sprintf("%s?browser_name=%s&browser_version=%s&os=%s&os_version=%s&real_mobile=%s&device=%s", browserstackURL,
+		browserName,
+		browserVersion,
+		os,
+		osVersion,
+		realMobile,
+		device)
+
+	plugin.VisitLocal(wd, requestURL)
 }
