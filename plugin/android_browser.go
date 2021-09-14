1a25c582ca55c4ae9c8ffef330e393ad02e028c3
deancn <dean@airdb.com>
2021-09-14 21:28:52 +0800

    add andoird

---
+++ plugin/android_browser.go
@@ -0,0 +1,72 @@
+package plugin
+
+const (
+	BrowserNameAndroid = "Android"
+
+	AndroidBrowserVersion4_4  = "4.4"
+	AndroidBrowserVersion5_0  = "5.0"
+	AndroidBrowserVersion6_0  = "6.0"
+	AndroidBrowserVersion7_0  = "7.0"
+	AndroidBrowserVersion8_0  = "8.0"
+	AndroidBrowserVersion8_1  = "8.1"
+	AndroidBrowserVersion9_0  = "9.0"
+	AndroidBrowserVersion10_0 = "10.0"
+	AndroidBrowserVersion11_0 = "11.0"
+)
+
+func GetBrowserAndroid() []string {
+	return []string{
+		AndroidBrowserVersion4_4,
+		AndroidBrowserVersion5_0,
+		AndroidBrowserVersion6_0,
+		AndroidBrowserVersion7_0,
+		AndroidBrowserVersion8_0,
+		AndroidBrowserVersion8_1,
+		AndroidBrowserVersion9_0,
+		AndroidBrowserVersion10_0,
+		AndroidBrowserVersion11_0,
+	}
+}
+
+const (
+	AndroidDeviceGalaxy         = "Samsung Galaxy A10"
+	AndoirdDeviceGalaxyA11      = "Samsung Galaxy A11"
+	AndoirdDeviceGalaxyA51      = "Samsung Galaxy A51"
+	AndoirdDeviceGalaxyA8       = "Samsung Galaxy A8"
+	AndoirdDeviceGalaxyJ7Prime  = "Samsung Galaxy J7 Prime"
+	AndoirdDeviceGalaxyNote10   = "Samsung Galaxy Note 10"
+	AndoirdDeviceGalaxy10Plus   = "Samsung Galaxy Note 10 Plus"
+	AndoirdDeviceGalaxyNote20   = "Samsung Galaxy Note 20"
+	AndoirdDeviceGalaxy20Ultra  = "Samsung Galaxy Note 20 Ultra"
+	AndoirdDeviceGalaxyNote8    = "Samsung Galaxy Note 8"
+	AndoirdDeviceGalaxyNote9    = "Samsung Galaxy Note 9"
+	AndoirdDeviceGalaxyS10      = "Samsung Galaxy S10"
+	AndoirdDeviceGalaxyS10Plus  = "Samsung Galaxy S10 Plus"
+	AndoirdDeviceGalaxyS10e     = "Samsung Galaxy S10e"
+	AndoirdDeviceGalaxyS20      = "Samsung Galaxy S20"
+	AndoirdDeviceGalaxyS20Plus  = "Samsung Galaxy S20 Plus"
+	AndoirdDeviceGalaxyS20Ultra = "Samsung Galaxy S20 Ultra"
+	AndoirdDeviceGalaxyS21      = "Samsung Galaxy S21"
+	AndoirdDeviceGalaxyS21Plus  = "Samsung Galaxy S21 Plus"
+	AndoirdDeviceGalaxyS21Ultra = "Samsung Galaxy S21 Ultra"
+	AndoirdDeviceGalaxyS6       = "Samsung Galaxy S6"
+	AndoirdDeviceGalaxyS7       = "Samsung Galaxy S7"
+	AndoirdDeviceGalaxyS8       = "Samsung Galaxy S8"
+	AndoirdDeviceGalaxyS8Plus   = "Samsung Galaxy S8 Plus"
+	AndoirdDeviceGalaxyS9       = "Samsung Galaxy S9"
+	AndoirdDeviceGalaxyS9Plus   = "Samsung Galaxy S9 Plus"
+	AndoirdDeviceGalaxyTab4     = "Samsung Galaxy Tab 4"
+	AndoirdDeviceGalaxyTabS3    = "Samsung Galaxy Tab S3"
+	AndoirdDeviceGalaxyTabS4    = "Samsung Galaxy Tab S4"
+	AndoirdDeviceGalaxyTabS5e   = "Samsung Galaxy Tab S5e"
+	AndoirdDeviceGalaxyTabS6    = "Samsung Galaxy Tab S6"
+	AndoirdDeviceGalaxyTabS7    = "Samsung Galaxy Tab S7"
+)
+
+func GetGalaxAndroid() {
+
+}
+
+func GetVovoAndoird() {
+
+}
