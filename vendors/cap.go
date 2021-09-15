a517cb1d04be8da8f08a1cc341c61fca3f758442
Kyan820815 <patrickchang820815@gmail.com>
2021-09-15 18:30:47 +0800

    20210915-1 refactor and build data source from all desktop and mobile devices

--- vendors/cap.go
+++ vendors/cap.go
@@ -1,85 +1,123 @@
 package vendors
 
 import (
-	"log"
 	"os"
 
 	"github.com/tebeka/selenium"
 )
 
+var browserstackHosts = os.Getenv("BROWSERSTACK_HOSTS")
+var browserstackRunLocal = os.Getenv("BROWSERSTACK_RUN_LOCAL")
+
+// https://www.browserstack.com/list-of-browsers-and-platforms/automate
+// device:https://www.browserstack.com/list-of-browsers-and-platforms/automate
+// https://www.browserstack.com/automate/capabilities
 func GetCapList() []selenium.Capabilities {
+
+	// capList := []selenium.Capabilities{}
+	// capList = append(capList, GetWindowsCapList()...)
+	// capList = append(capList, GetMacCapList()...)
+	// capList = append(capList, GetAndroidCapList()...)
+	// capList = append(capList, GetIosCapList()...)
+	// return capList
+
+	return GetWindowsCapList()
+	// return GetMacCapList()
+	// return GetAndroidCapList()
+	// return GetIosCapList()
+}
+
+func GetWindowsCapList() []selenium.Capabilities {
+	allWindowsOSVersion := GetAllWindowsOSVersion()
+	return GetDesktopCapList(allWindowsOSVersion, WindowsOs)
+}
+
+func GetMacCapList() []selenium.Capabilities {
+	allMacOSVersion := GetAllMacOSVersion()
+	return GetDesktopCapList(allMacOSVersion, MacOs)
+}
+
+func GetIosCapList() []selenium.Capabilities {
+	allIosDevice := GetAllIosDevice()
+	allIosOSVersion := GetAllIosOSVersion()
+	return GetMobileCaplist(allIosDevice, allIosOSVersion, IosBrowserName, IosRealMobile)
+}
+
+func GetAndroidCapList() []selenium.Capabilities {
+	allAndroidDevice := GetAllAndroidDevice()
+	allAndroidOSVersion := GetAllAndroidOSVersion()
+	return GetMobileCaplist(allAndroidDevice, allAndroidOSVersion, AndroidBrowserName, AndroidRealMobile)
+}
+
+func GetDesktopCapList(allOSVerion []string, os string) []selenium.Capabilities {
 	var capList []selenium.Capabilities
 
-	browserstackHosts := os.Getenv("BROWSERSTACK_HOSTS")
-	browserstackRunLocal := os.Getenv("BROWSERSTACK_RUN_LOCAL")
+	allBrowserName := GetAllBrowserName()
 
-	// device:https://www.browserstack.com/list-of-browsers-and-platforms/automate
-	// https://www.browserstack.com/automate/capabilities
-	// https://www.browserstack.com/docs/automate/selenium/select-browsers-and-devices#selenium-capabilities
-
-	os := OSWindows
-	osVersion := OSWindowsVersion8_1
-
-	browserName := OSWindowsBrowerFirefox
-	browserVersion := OSWindowsBrowerFirefoxVersion88_0
-
-	realMobile := "false"
-	device := ""
-
-	caps := selenium.Capabilities{
-		"build":              "auto-test",
-		"name":               "send testing",
-		"project":            "auto-test",
-		"browserstack.local": browserstackRunLocal,
-		// "browserstack.debug":  "true",
-		// "browserstack.tunnel": "true",
-		"browserstack.hosts": browserstackHosts,
-		"browserName":        browserName,    // for pc
-		"browserVersion":     browserVersion, // for pc
-		"os":                 os,             // for pc
-		"os_version":         osVersion,      // for pc and mobile
-		"RealMobile":         realMobile,     // for mobile
-		"Device":             device,         // for mobile
+	for _, browserName := range allBrowserName {
+		switch browserName {
+		case BrowserFirefox:
+			allBrowserVersion := GetAllBrowserVersionFirefox()
+			capList = append(capList, GetDesktopCapListCore(allOSVerion, allBrowserVersion, os, browserName)...)
+		case BrowserChrome:
+			allBrowserVersion := GetAllBrowserVersionChrome()
+			capList = append(capList, GetDesktopCapListCore(allOSVerion, allBrowserVersion, os, browserName)...)
+		case BrowserIE:
+			allBrowserVersion := GetAllBrowserVersionIE()
+			capList = append(capList, GetDesktopCapListCore(allOSVerion, allBrowserVersion, os, browserName)...)
+		case BrowserOpera:
+			allBrowserVersion := GetAllBrowserVersionOpera()
+			capList = append(capList, GetDesktopCapListCore(allOSVerion, allBrowserVersion, os, browserName)...)
+		case BrowserSafari:
+			allBrowserVersion := GetAllBrowserVersionSafari()
+			capList = append(capList, GetDesktopCapListCore(allOSVerion, allBrowserVersion, os, browserName)...)
+		default:
+		}
 	}
-
-	capList = append(capList, caps)
 
 	return capList
 }
 
-func GetWindowsCapList() {
-	GetWindowsFirefoxCapList()
-	GetWindowsChromeCapList()
-}
-
-func GetWindowsFirefoxCapList() {
-}
-
-func GetWindowsChromeCapList() {
-}
-
-func GetAndoirdCapList() {
+func GetDesktopCapListCore(allOSVersion, allBrowserVersion []string, os, browserName string) []selenium.Capabilities {
 	var capList []selenium.Capabilities
-	cap := selenium.Capabilities{
-		"build":   "auto-test",
-		"name":    "send testing",
-		"project": "auto-test",
-	}
-
-	for _, osVersion := range AllAndroidOSVersion {
-		for _, browserName := range AllAndroidBrowserName {
+	for _, osVersion := range allOSVersion {
+		for _, browserVersion := range allBrowserVersion {
+			cap := selenium.Capabilities{
+				"build":               os,
+				"name":                "testing",
+				"project":             "auto-test",
+				"browserstack.tunnel": "true",
+				"browserstack.local":  browserstackRunLocal,
+				"browserstack.hosts":  browserstackHosts,
+			}
+			cap["os"] = os
 			cap["os_version"] = osVersion
 			cap["browserName"] = browserName
-			// log.Println(cap)
+			cap["browserVersion"] = browserVersion
 			capList = append(capList, cap)
 		}
 	}
-
-	log.Println(capList)
+	return capList
 }
 
-func GetIPadCapList() {
-}
-
-func GetIPhoneCaplist() {
+func GetMobileCaplist(allDevice, allOSVersion []string, browserName, realMobile string) []selenium.Capabilities {
+	var capList []selenium.Capabilities
+	for _, device := range allDevice {
+		for _, osVersion := range allOSVersion {
+			cap := selenium.Capabilities{
+				"build":               browserName,
+				"name":                "testing",
+				"project":             "auto-test",
+				"browserstack.tunnel": "true",
+				"browserstack.local":  browserstackRunLocal,
+				"browserstack.hosts":  browserstackHosts,
+			}
+			cap["os_version"] = osVersion
+			cap["browserName"] = browserName
+			cap["realMobile"] = realMobile
+			cap["device"] = device
+			capList = append(capList, cap)
+		}
+	}
+	return capList
 }
--- vendors/cap_test.go
+++ vendors/cap_test.go
@@ -2,13 +2,10 @@ package vendors_test
 
 import (
 	"testing"
-
-	"github.com/airdb/go-browserstack/vendors"
 )
 
-func TestGetAndoirdCapList(t *testing.T) {
+func TestGetCapList(t *testing.T) {
 	t.Log("start testing...")
-	vendors.GetAndoirdCapList()
 
 	t.Log("test finished")
 }
